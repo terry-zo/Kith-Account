00fb9934ed43d45a33b310bf554c12f948a240a4
Terry <38963056+zoegodterry@users.noreply.github.com>
2018-05-04 02:28:45 -0400

    major changes to gen

--- gen.py
+++ gen.py
@@ -12,18 +12,11 @@ import sys
 import json
 import bs4
 from bs4 import BeautifulSoup as soup
+from threading import Thread, Lock
 from random import randint
-
-registered = 0
 
 
 def log(phrase):
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-    print("Kith Account Generator")
-    print("Twitter @zoegodterry\n")
     with open('log.txt', 'a+') as logfile:
         logfile.write(phrase + "\n")
         logfile.close()
@@ -46,37 +39,37 @@ def genemail(rawemail):
 def verifydata(config):
     for data in config:
         if config[data] == "":
-            log(data + " is not filled out in config.json! Exiting...")
+            log("{} is not filled out in config.json! Exiting...".format(data))
             sys.exit()
 
 
-def request_recaptcha(service_key, google_site_key, pageurl):
+def request_recaptcha(service_key, google_site_key, pageurl, index):
     url = "http://2captcha.com/in.php?key=" + service_key + "&method=userrecaptcha&googlekey=" + google_site_key + "&pageurl=" + pageurl
     resp = requests.get(url)
     if resp.text[0:2] != 'OK':
-        log('Error: ' + resp.text + "\nExiting...")
+        log("#{} - Error: {} Exiting...".format(index. resp.text))
         sys.exit()
     captcha_id = resp.text[3:]
-    log("Successfully Requested For Captcha!")
-    time.sleep(0.1)
+    log("#{} - Successfully requested for captcha.".format(index))
     return captcha_id
 
 
-def receive_token(captcha_id, service_key):
+def receive_token(captcha_id, service_key, index):
     fetch_url = "http://2captcha.com/res.php?key=" + service_key + "&action=get&id=" + captcha_id
-    for count, i in enumerate(range(1, 80)):
-        log('Getting Captcha Token Attempt #' + str(count) + "/80")
+    for count in range(1, 26):
+        log("#{} - Attempting to fetch token. {}/25".format(index, count))
         resp = requests.get(fetch_url)
         if resp.text[0:2] == 'OK':
             grt = resp.text.split('|')[1]  # g-recaptcha-token
-            log("Captcha Token Received: " + grt)
-            time.sleep(0.1)
+            log("#{} - Captcha token received.".format(index))
             return grt
-        time.sleep(1)
-    log("No Tokens Received. Restarting...")
-    time.sleep(1)
+        time.sleep(5)
+    log("#{} - No tokens received. Restarting...".format(index))
+    global numofaccs
+    global retry
     global config
-    genaccs(config)
+    retry += 1
+    main(numofaccs, config, retry)
 
 
 def submit_recaptcha(grt, at, session):
@@ -90,7 +83,7 @@ def submit_recaptcha(grt, at, session):
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9",
         "Cache-Control": "max-age=0",
-        "Connection": "keep-alive",
+        "Connection": "close",
         "Content-Length": "614",
         "Content-Type": "application/x-www-form-urlencoded",
         "Host": " kith.com",
@@ -102,19 +95,23 @@ def submit_recaptcha(grt, at, session):
     return resp
 
 
-def grabauthkey(page_html):
+def grabauthkey(page_html, index):
     page_soup = soup(page_html, 'html.parser')
     authtokenvar = page_soup.findAll("input", {"name": "authenticity_token"})
     if authtokenvar:
         authtoken = authtokenvar[0]["value"]
-        log("Authenticity token found: " + authtoken)
+        log("#{} - Authenticity token found.".format(index))
         return authtoken
     else:
-        log("No authenticity token found. Exiting...")
-        sys.exit()
+        log("#{} - No authenticity token found. Restarting...".format(index))
+        global numofaccs
+        global retry
+        global config
+        retry += 1
+        main(numofaccs, config, retry)
 
 
-def genaccs(config):
+def genaccs(config, index):
     s = requests.Session()
     fn = config['firstname']
     ln = config['lastname']
@@ -134,7 +131,7 @@ def genaccs(config):
         'Accept-Encoding': 'gzip, deflat    e, br',
         'Accept-Language': 'en-US,en;q=0.9',
         'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
+        'Connection': 'close',
         'Content-Length': '205',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Host': 'kith.com',
@@ -144,31 +141,50 @@ def genaccs(config):
     }
     b = s.post('https://kith.com/account', headers=headers, data=payload, allow_redirects=False)
     if b.status_code == 302:
-        log('Requesting Captcha...')
-        captcha_id = request_recaptcha(config['captchakey'], config['sitekey'], 'https://kith.com/challenge')
-        grt = receive_token(captcha_id, config['captchakey'])
+        log('#{} - Requesting Captcha...'.format(index))
+        captcha_id = request_recaptcha(config['captchakey'], config['sitekey'], 'https://kith.com/challenge', index)
+        grt = receive_token(captcha_id, config['captchakey'], index)
         challenge = s.get('https://kith.com/challenge')
         challenge_html = challenge.content
-        authtoken = grabauthkey(challenge_html)
+        authtoken = grabauthkey(challenge_html, index)
         submit_recaptcha(grt, authtoken, s)
     else:
-        log('An unexpected ' + str(b.status_code) + ' error has occurred. Exiting...')
+        log('#{} - An unexpected ' + str(b.status_code) + ' error has occurred. Exiting...'.format(index))
         sys.exit()
-    global registered
-    registered += 1
-    log('Returned Status Code: ' + str(b.status_code) + '\nFirst Name: ' + fn + '\nLast Name: ' + ln + '\nE-mail: ' + email + '\nPassword: ' + pw + '\nSuccessfully registered account #' + str(registered))
+    log('#{} - Successfully registered.'.format(index))
     with open('Accounts.txt', 'a+') as txtfile:
         txtfile.write(email + ':' + pw + "\n")
         txtfile.close()
-    s.cookies.clear()
+    s.close()
     time.sleep(interval)
 
 
+def main(numofaccs, config, retry=0):
+    global thread_list
+    if retry == 0:
+        for index in range(numofaccs):
+            t = Thread(target=genaccs, name=index, args=(config, index))
+            t.start()
+            thread_list.append(t)
+            log("#{} - Creating account".format(index))
+        for t in thread_list:
+            t.join()
+    else:
+        index = numofaccs + retry
+        t = Thread(target=genaccs, name=index, args=(config, index))
+        t.start()
+        thread_list.append(t)
+        log("#{} - Creating account".format(index))
+        for t in thread_list:
+            t.join()
+
+
+retry = 0
+thread_list = []
 config = readconfig('config.json')
 verifydata(config)
 numofaccs = config["numofaccounts"]
-log("Creating " + str(numofaccs) + " accounts...")
-for _ in range(numofaccs):
-    genaccs(config)
-log("Successfully created " + str(numofaccs) + " accounts!")
+start = time.time()
+main(numofaccs, config)
+log("Finished {} accounts in {} seconds!".format(numofaccs, time.time() - start))
 sys.exit()
