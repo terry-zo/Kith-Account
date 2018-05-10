30c2c775fcb9d12fa73ca848bfd322dc726fefe6
Terry <38963056+zoegodterry@users.noreply.github.com>
2018-05-09 20:37:08 -0400

    bug fix
    added raise

--- gen.py
+++ gen.py
@@ -85,8 +85,9 @@ def request_recaptcha(service_key, google_site_key, pageurl, index):
     resp = requests.get(url)
     if resp.text[0:2] != 'OK':
         log("#{} - Error: {} Exiting...".format(index, resp.text))
+        raise
     captcha_id = resp.text[3:]
-    log("#{} - Successfully requested for captcha.".format(index))
+    print("#{} - Successfully requested for captcha.".format(index))
     return captcha_id
 
 
@@ -94,16 +95,17 @@ def receive_token(captcha_id, service_key, index):
     global queue_, lock_
     fetch_url = "http://2captcha.com/res.php?key=" + service_key + "&action=get&id=" + captcha_id
     for count in range(1, 26):
-        log("#{} - Attempting to fetch token. {}/25".format(index, count))
+        print("#{} - Attempting to fetch token. {}/25".format(index, count))
         resp = requests.get(fetch_url)
         if resp.text[0:2] == 'OK':
             grt = resp.text.split('|')[1]  # g-recaptcha-token
-            log("#{} - Captcha token received.".format(index))
+            print("#{} - Captcha token received.".format(index))
             return grt
         time.sleep(5)
-    log("#{} - No tokens received. Restarting...".format(index))
+    print("#{} - No tokens received. Restarting...".format(index))
     with lock_:
         queue_.put(1)
+    raise
 
 
 def submit_recaptcha(grt, at, session, rand_proxy):
@@ -134,72 +136,77 @@ def grabauthkey(page_html, index):
     authtokenvar = page_soup.findAll("input", {"name": "authenticity_token"})
     if authtokenvar:
         authtoken = authtokenvar[0]["value"]
-        log("#{} - Authenticity token found.".format(index))
+        print("#{} - Authenticity token found.".format(index))
         return authtoken
     else:
         log("#{} - No authenticity token found. Restarting...".format(index))
         with lock_:
             queue_.put(1)
+        raise
 
 
 def genaccs(config, index):
     global queue_, lock_, p_list, p_list_lock, p_lock, a_lock
     while queue_.qsize() > 0:
-        with lock_:
-            queue_.get()
-        rand_proxy = choice(p_list)
-        if not rand_proxy in p_list_lock:
-            if rand_proxy != None:
-                with p_lock:
-                    p_list_lock.append(rand_proxy)
-                    log("Using proxy: " + str(rand_proxy))
-            s = requests.Session()
-            fn = config['firstname']
-            ln = config['lastname']
-            pw = config['password']
-            interval = config['interval']
-            email = genemail(config['email'])
-            payload = {
-                'form_type': 'create_customer',
-                "customer[first_name]": fn,
-                "customer[last_name]": ln,
-                "customer[email]": email,
-                "customer[password]": pw
-            }
-            headers = {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Cache-Control': 'max-age=0',
-                'Connection': 'close',
-                'Content-Length': '205',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Host': 'kith.com',
-                'Referer': 'https://kith.com/account/register',
-                'Upgrade-Insecure-Requests': '1',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
-            }
-            b = s.post('https://kith.com/account', headers=headers, data=payload, allow_redirects=False, proxies={"https": rand_proxy}, timeout=30)
-            if b.status_code == 302:
-                log('#{} - Requesting Captcha...'.format(index))
-                captcha_id = request_recaptcha(config['captchakey'], config['sitekey'], 'https://kith.com/challenge', index)
-                grt = receive_token(captcha_id, config['captchakey'], index)
-                challenge = s.get('https://kith.com/challenge')
-                challenge_html = challenge.content
-                authtoken = grabauthkey(challenge_html, index)
-                submit_recaptcha(grt, authtoken, s, rand_proxy)
-            else:
-                log('#{} - An unexpected ' + str(b.status_code) + ' error has occurred.'.format(index))
-            log('#{} - Successfully registered.'.format(index))
-            with a_lock:
-                with open('Accounts.txt', 'a+') as txtfile:
-                    txtfile.write(email + ':' + pw + "\n")
-            s.close()
-            unlock_p(rand_proxy)
-            time.sleep(interval)
-        else:
+        try:
             with lock_:
-                queue_.put(1)
+                queue_.get()
+            rand_proxy = choice(p_list)
+            if not rand_proxy in p_list_lock:
+                if rand_proxy != None:
+                    with p_lock:
+                        p_list_lock.append(rand_proxy)
+                        print("Using proxy: " + str(rand_proxy))
+                s = requests.Session()
+                fn = config['firstname']
+                ln = config['lastname']
+                pw = config['password']
+                interval = config['interval']
+                email = genemail(config['email'])
+                payload = {
+                    'form_type': 'create_customer',
+                    "customer[first_name]": fn,
+                    "customer[last_name]": ln,
+                    "customer[email]": email,
+                    "customer[password]": pw
+                }
+                headers = {
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Cache-Control': 'max-age=0',
+                    'Connection': 'close',
+                    'Content-Length': '205',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Host': 'kith.com',
+                    'Referer': 'https://kith.com/account/register',
+                    'Upgrade-Insecure-Requests': '1',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
+                }
+                b = s.post('https://kith.com/account', headers=headers, data=payload, allow_redirects=False, proxies={"https": rand_proxy}, timeout=30)
+                if b.status_code == 302:
+                    print('#{} - Requesting Captcha...'.format(index))
+                    captcha_id = request_recaptcha(config['captchakey'], config['sitekey'], 'https://kith.com/challenge', index)
+                    grt = receive_token(captcha_id, config['captchakey'], index)
+                    challenge = s.get('https://kith.com/challenge')
+                    challenge_html = challenge.content
+                    authtoken = grabauthkey(challenge_html, index)
+                    submit_recaptcha(grt, authtoken, s, rand_proxy)
+                else:
+                    log('#{} - An unexpected ' + str(b.status_code) + ' error has occurred.'.format(index))
+                    raise
+                log('#{} - Successfully registered.'.format(index))
+                with a_lock:
+                    with open('Accounts.txt', 'a+') as txtfile:
+                        txtfile.write(email + ':' + pw + "\n")
+                s.close()
+                unlock_p(rand_proxy)
+                time.sleep(interval)
+            else:
+                with lock_:
+                    queue_.put(1)
+        except Exception:
+            pass
 
 
 def unlock_p(rand_proxy):
@@ -207,7 +214,7 @@ def unlock_p(rand_proxy):
     if (rand_proxy in p_list_lock) and (rand_proxy != None):
         with p_lock:
             p_list_lock.remove(rand_proxy)
-            log("Released proxy: " + str(rand_proxy))
+            print("Released proxy: " + str(rand_proxy))
 
 
 def main(numofaccs, config):
